f656b8d41b27bf16c787e9dc6186f715618db489
Tyom Semonov <inspiro@gmail.com>
2013-01-26 14:48:18 +0000

    Stratum Gem. Added more options
    install (force), update, remove

--- lib/stratum.rb
+++ lib/stratum.rb
@@ -7,11 +7,36 @@ module Stratum
 	class Generator < Thor
 		map ['-v', '--version'] => :version
 
-		desc 'install', 'Install Stratum into current directory'
-		method_options :path => :string
+		desc 'install', 'Install Stratum (./stratum); `stratum help install` for options'
+		option :path, :type => :string, :aliases => '-p', :banner => './', :desc => 'Installation path'
+		option :force, :aliases => '-f', :type => :boolean, :desc => 'Force installation (overwrite existing)'
 		def install
-			copy_files
-			puts "Stratum installed to #{install_path}"
+			if install_path.exist? && !options[:force]
+				puts "Stratum installation already exists. Use -f to force"
+			else
+				copy_files
+				puts "Stratum installed to #{install_path}"
+			end
+		end
+
+		desc 'update', 'Update Stratum'
+		def update
+			remove(true)
+		end
+
+		desc 'remove', 'Remove Stratum'
+		def remove(replace = false)
+			if stratum_exists?
+				FileUtils.rm_rf("stratum")
+				if replace
+					copy_files
+					puts "Stratum updated"
+				else
+					puts "Stratum has been removed"
+				end
+			else
+				puts "Stratum installation is not found in current directory"
+			end
 		end
 
 		desc 'version', 'Show Stratum version'
@@ -20,6 +45,10 @@ module Stratum
 		end
 
 		private
+
+		def stratum_exists?
+			install_path.exist?
+		end
 
 		def install_path
 			@install_path ||= if options[:path]
@@ -31,10 +60,10 @@ module Stratum
 
 		def copy_files
 			FileUtils.mkdir_p(install_path)
-			FileUtils.cp_r(stratum_stylesheets, install_path)
+			FileUtils.cp_r(assets_stylesheets, install_path)
 		end
 
-		def stratum_stylesheets
+		def assets_stylesheets
 			current_dir = File.dirname(File.dirname(__FILE__))
 			stylesheets = File.join(current_dir, "assets", "stylesheets")
 
